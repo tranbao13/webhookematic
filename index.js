6223f4abeb95033ed5f08929e145d526152535ab
tranbao13 <nguyen.tranbao13@gmail.com>
2020-05-14 20:28:18 +0700

    initiate webhook

---
+++ index.js
@@ -0,0 +1,4 @@
+// This file is used to register all your cloud functions in GCP.
+// DO NOT EDIT/DELETE THIS, UNLESS YOU KNOW WHAT YOU ARE DOING.
+
+exports.webhookematic = require("./webhookematic.js").handler;
